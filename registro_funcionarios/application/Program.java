722c3bcbda97676686cbc93ee4f37aa0065dbf3e
Yves Muniz <yvesmuniz@gmail.com>
2020-06-13 10:37:27 -0300

    add ex registro de funcionarios

---
+++ registro_funcionarios/application/Program.java
@@ -0,0 +1,61 @@
+package funcionarios.registro.application;
+
+import funcionarios.registro.entities.Employee;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Employee> list = new ArrayList<>();
+
+        System.out.print("How many employees will be registered? ");
+        int n = sc.nextInt();
+        for (int i=1; i<=n; i++) {
+            System.out.println();
+            System.out.println("Employee #" + i + ": ");
+            System.out.print("Id: ");
+            int id = sc.nextInt();
+            while (hasId(list, id)) {
+                System.out.print("Id already taken. Try again: ");
+                id = sc.nextInt();
+            }
+
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Salary: ");
+            double salary = sc.nextDouble();
+            list.add(new Employee(id, name, salary));
+        }
+
+        System.out.println();
+        System.out.print("Enter the employee id that will have salary increase: ");
+        int id = sc.nextInt();
+        Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+        if (emp == null) {
+            System.out.println("This id does not exist!");
+        }
+        else {
+            System.out.print("Enter the percentage: ");
+            double percentage = sc.nextDouble();
+            emp.increaseSalary(percentage);
+        }
+
+        System.out.println();
+        System.out.println("List of employees:");
+        for (Employee obj : list) {
+            System.out.println(obj);
+        }
+        sc.close();
+    }
+
+    public static boolean hasId(List<Employee> list, int id) {
+        Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+        return emp != null;
+    }
+}
